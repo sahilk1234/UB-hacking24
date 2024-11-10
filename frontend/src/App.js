6306b6deab6d37112919ddb9ca88bbd0eb29229a
LAPTOP-NB3KLFFF\sgupt <sgupta98mnit@gmail.com>
2024-11-10 02:14:24 -0500

    Profile changes fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Profile from './components/Profile';
 import ChatPage from './components/ChatPage';
 import './App.css'
 
 function App() {
   return (
-    <Router>
+    <AuthProvider>
+        <Router>
       <div className="App">
         <Routes>
           <Route path="/profile" element={<Profile />} />
@@ -15,6 +17,8 @@ function App() {
         </Routes>
       </div>
     </Router>
+    </AuthProvider>
+    
   );
 }
 
